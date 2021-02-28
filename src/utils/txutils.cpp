de2b800c0c214cf5b8147838121fea76ee98f465
grotsztaksel <grotsztaksel@o2.pl>
2021-02-28 09:07:52 +0100

    Utils:Tixi: Add more basic tixi functions

--- src/utils/txutils.cpp
+++ src/utils/txutils.cpp
@@ -1,8 +1,53 @@
-#include <stdexcept>
 #include "txutils.h"
+#include <stdexcept>
+#include "general.h"
+
+#include <string.h>
+
+typedef std::string::size_type istr;
 
 void txutils::handle_error(ReturnCode code) {
   if (code == INVALID_HANDLE) {
     throw std::runtime_error("Invalid Tixi Handle!");
   }
+}
+
+ReturnCode txutils::indexedPath(TixiDocumentHandle handle,
+                                const char* xPathExpression,
+                                int index,
+                                char** ipath) {
+  char* xPath;
+  ReturnCode ret =
+      tixiXPathExpressionGetXPath(handle, xPathExpression, index, &xPath);
+  if (ret != ReturnCode::SUCCESS) {
+    return ret;
+  }
+
+  // std::string offers way more tools to process the path than just char.
+  const std::string& path = std::string(xPath);
+  std::vector<std::string> elements = split(path, '/');
+
+  return ReturnCode::SUCCESS;
+}
+
+char* txutils::elementName(const char* xPathExpression) {
+  const std::string path = std::string(uniqueName(xPathExpression));
+  istr i = path.find_first_of("[");
+  return strdup(path.substr(0, i).c_str());
+}
+
+int txutils::elementNumber(const char* xPathExpression) {
+  const std::string path = std::string(uniqueName(xPathExpression));
+  istr i = path.find_first_of("[");
+  if (i == std::string::npos) {
+    return 1;
+  }
+  istr n = path.find_first_of("]");
+  return std::stoi(path.substr(i + 1, n - i - 1));
+}
+
+char* txutils::uniqueName(const char* xPathExpression) {
+  const std::string path = std::string(xPathExpression);
+  istr i = path.find_last_of("/") + 1;
+  return strdup(path.substr(i).c_str());
 }
--- src/utils/txutils.h
+++ src/utils/txutils.h
@@ -9,6 +9,33 @@ namespace txutils {
 // Throw error when INVALID_HANDLE
 void handle_error(ReturnCode code);
 
-};  // namespace tixi_utils
+/**
+  @brief Evaluates a XPath expression and the xPath for the i-th result and
+  converts it to a format composed only of indexes: "/*[1]/*[i]/*[j]/*[k]..."
+
+  @param[in]  handle handle as returned by ::tixiOpenDocument,
+  ::tixiOpenDocumentRecursive, ::tixiOpenDocumentFromHTTP, ::tixiCreateDocument
+  or ::tixiImportFromString
+  @param[in]  xPathExpression The XPath Expression to evaluate.
+  @param[in]  index The index of the result to query, with 1 <= index <= number,
+  and number queried with ::tixiXPathEvaluateNodeNumber
+  @param[out] xPath The xPath of the search result in the "index" format.
+
+  @return
+    - SUCCESS if successfully retrieve the xPath of the search.
+    - INVALID_HANDLE if the handle is not valid, i.e.  does not or no longer
+  exist.
+    - FAILED  if an internal error occured.
+ */
+ReturnCode indexedPath(TixiDocumentHandle handle,
+                       const char* xPathExpression,
+                       int index,
+                       char** ipath);
+
+char* elementName(const char* xPathExpression);
+int elementNumber(const char* xPathExpression);
+char* uniqueName(const char* xPathExpression);
+
+};  // namespace txutils
 
 #endif  // TIXI_UTILS_H
