57ed7816e426a988e394c12db2c474d14b1f4ab6
grotsztaksel <grotsztaksel@o2.pl>
2021-03-06 21:16:47 +0100

    QTX3Node_test: Use dedicated test model, move everything to cpp

--- test/model/qxtreenodetest.cpp
+++ test/model/qxtreenodetest.cpp
@@ -1,18 +1,112 @@
-#include "qxtreenodetest.h"
+﻿#include "qxtreenodetest.h"
 
 #include <QObject>
 #include "qtx3model.h"
 #include "qtx3node.h"
-QXTreeNodeTest::QXTreeNodeTest() {
-  model = new QTX3Model();
 
-  node = new QTX3Node(qobject_cast<QObject*>(model));
+#include <regex>
+
+/***************************************************
+ *
+ *
+ *    Special model used to test the nodes
+ *
+ *
+ ***************************************************/
+class QTX3TestModel : public QTX3Model {
+ public:
+  QTX3TestModel(QObject* parent = nullptr);
+};
+
+/*-------------------------------*
+ *
+ * ---model class methods---
+ *
+ *-------------------------------*/
+QTX3TestModel::QTX3TestModel(QObject* parent) : QTX3Model(parent) {
+  //  std::string rawstring(R"(<?xml version="1.0"?>
+  //                          <root>
+  //                              <child_1>
+  //                                  <child/>
+  //                              </child_1>
+  //                              <child_2 attr="foo" name="bar">
+  //                                  <child_2 attr="9">
+  //                                      <node_3>
+  //                                          <node_4/>
+  //                                          <!-- some comment -->
+  //                                          <node_4 attr="good"/>
+  //                                          <node_5>
+  //                                                              Text O
+  //                                          </node_5>
+  //                                          <node_4 name="node4">
+  //                                                              Text
+  //                                          </node_4>
+  //                                      </node_3>
+  //                                      <node_3 name="none"/>
+  //                                  </child_2>
+  //                                  <child_2/>
+  //                                  <node_3>
+  //                                      <node_4/>
+  //                                  </node_3>
+  //                              </child_2>
+  //                              <child_2>
+  //                                  <node_3>
+  //                                      <node_4/>
+  //                                      <node_4/>
+  //                                      <node_5/>
+  //                                  </node_3>
+  //                              </child_2>
+  //                          </root>)");
+  //  std::regex re(R"(>\s*<)");
+  //  std::string stripped = std::regex_replace(rawstring, re, "><");
+  //  const char* xml = strdup(stripped.c_str());
+  //  tixiImportFromString(xml, &_tixihandle);
+}
+
+/***************************************************
+ *
+ *
+ *    THE TEST CLASS
+ *
+ *
+ ***************************************************/
+class QXTreeNodeTest : public ::testing::Test {
+ public:
+  QXTreeNodeTest();
+  ~QXTreeNodeTest();
+
+ protected:
+  //  void SetUp() override;
+  //  void TearDown() override;
+
+ protected:
+  QTX3Node* node;
+  QTX3Model* model;
+};
+
+/*-------------------------------*
+ *
+ * ---test class methods---
+ *
+ *-------------------------------*/
+
+QXTreeNodeTest::QXTreeNodeTest() {
+  model = new QTX3TestModel();
+  node = new QTX3Node(model);
 }
 
 QXTreeNodeTest::~QXTreeNodeTest() {
   node->deleteLater();
   model->deleteLater();
 }
+
+// void QXTreeNodeTest::SetUp() {
+
+//}
+
+// void QXTreeNodeTest::TearDown() {
+//  model->deleteLater();
+//}
 
 TEST_F(QXTreeNodeTest, test_xPath) {
   ASSERT_EQ(QString("/*[1]"), node->xPath());
