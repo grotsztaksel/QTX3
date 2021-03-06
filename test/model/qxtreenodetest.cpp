aa71ee89e186c50d4419fa4f46bb3cb9815f4610
grotsztaksel <grotsztaksel@o2.pl>
2021-03-06 21:29:41 +0100

    QTX3Node_test: Add another test

--- test/model/qxtreenodetest.cpp
+++ test/model/qxtreenodetest.cpp
@@ -24,43 +24,43 @@ class QTX3TestModel : public QTX3Model {
  *
  *-------------------------------*/
 QTX3TestModel::QTX3TestModel(QObject* parent) : QTX3Model(parent) {
-  //  std::string rawstring(R"(<?xml version="1.0"?>
-  //                          <root>
-  //                              <child_1>
-  //                                  <child/>
-  //                              </child_1>
-  //                              <child_2 attr="foo" name="bar">
-  //                                  <child_2 attr="9">
-  //                                      <node_3>
-  //                                          <node_4/>
-  //                                          <!-- some comment -->
-  //                                          <node_4 attr="good"/>
-  //                                          <node_5>
-  //                                                              Text O
-  //                                          </node_5>
-  //                                          <node_4 name="node4">
-  //                                                              Text
-  //                                          </node_4>
-  //                                      </node_3>
-  //                                      <node_3 name="none"/>
-  //                                  </child_2>
-  //                                  <child_2/>
-  //                                  <node_3>
-  //                                      <node_4/>
-  //                                  </node_3>
-  //                              </child_2>
-  //                              <child_2>
-  //                                  <node_3>
-  //                                      <node_4/>
-  //                                      <node_4/>
-  //                                      <node_5/>
-  //                                  </node_3>
-  //                              </child_2>
-  //                          </root>)");
-  //  std::regex re(R"(>\s*<)");
-  //  std::string stripped = std::regex_replace(rawstring, re, "><");
-  //  const char* xml = strdup(stripped.c_str());
-  //  tixiImportFromString(xml, &_tixihandle);
+  std::string rawstring(R"(<?xml version="1.0"?>
+                            <root>
+                                <child_1>
+                                    <child/>
+                                </child_1>
+                                <child_2 attr="foo" name="bar">
+                                    <child_2 attr="9">
+                                        <node_3>
+                                            <node_4/>
+                                            <!-- some comment -->
+                                            <node_4 attr="good"/>
+                                            <node_5>
+                                                                Text O
+                                            </node_5>
+                                            <node_4 name="node4">
+                                                                Text
+                                            </node_4>
+                                        </node_3>
+                                        <node_3 name="none"/>
+                                    </child_2>
+                                    <child_2/>
+                                    <node_3>
+                                        <node_4/>
+                                    </node_3>
+                                </child_2>
+                                <child_2>
+                                    <node_3>
+                                        <node_4/>
+                                        <node_4/>
+                                        <node_5/>
+                                    </node_3>
+                                </child_2>
+                            </root>)");
+  std::regex re(R"(>\s*<)");
+  std::string stripped = std::regex_replace(rawstring, re, "><");
+  const char* xml = strdup(stripped.c_str());
+  tixiImportFromString(xml, &_tixihandle);
 }
 
 /***************************************************
@@ -72,12 +72,9 @@ QTX3TestModel::QTX3TestModel(QObject* parent) : QTX3Model(parent) {
  ***************************************************/
 class QXTreeNodeTest : public ::testing::Test {
  public:
-  QXTreeNodeTest();
-  ~QXTreeNodeTest();
-
  protected:
-  //  void SetUp() override;
-  //  void TearDown() override;
+  void SetUp() override;
+  void TearDown() override;
 
  protected:
   QTX3Node* node;
@@ -90,24 +87,21 @@ class QXTreeNodeTest : public ::testing::Test {
  *
  *-------------------------------*/
 
-QXTreeNodeTest::QXTreeNodeTest() {
+void QXTreeNodeTest::SetUp() {
   model = new QTX3TestModel();
   node = new QTX3Node(model);
 }
 
-QXTreeNodeTest::~QXTreeNodeTest() {
-  node->deleteLater();
-  model->deleteLater();
+void QXTreeNodeTest::TearDown() {
+  delete node;
+  delete model;
 }
-
-// void QXTreeNodeTest::SetUp() {
-
-//}
-
-// void QXTreeNodeTest::TearDown() {
-//  model->deleteLater();
-//}
 
 TEST_F(QXTreeNodeTest, test_xPath) {
   ASSERT_EQ(QString("/*[1]"), node->xPath());
+}
+
+TEST_F(QXTreeNodeTest, test_Constructor_with_model) {
+  ASSERT_EQ(model, node->model());
+  ASSERT_EQ(model, node->parent());
 }
