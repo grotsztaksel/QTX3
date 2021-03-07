fe835ee6345080c212eb6ff7ef5870d8c6ff0eb9
grotsztaksel <grotsztaksel@o2.pl>
2021-03-07 02:10:11 +0100

    QTX3Model_test: Successfully run a simple QTest suite

--- test/model/qtest_qtx3model.cpp
+++ test/model/qtest_qtx3model.cpp
@@ -1,10 +1,17 @@
-#include <QObject>
 
-class QTX3ModelTest : public QObject {
-  Q_OBJECT
- public:
-  explicit QTX3ModelTest(QObject* parent = nullptr);
+#include "qtest_qtx3model.h"
+#include <QtTest/QAbstractItemModelTester>
+#include <QtTest/QTest>
+#include "qtx3testcasemodel.h"
 
- signals:
-};
-QTX3ModelTest::QTX3ModelTest(QObject* parent) : QObject(parent) {}
+void QTX3ModelTest::test_anything() {
+  QCOMPARE(1, 1);
+}
+
+// void QTX3ModelTest::TestModel() {
+//  //  QTX3TestcaseModel* model = new QTX3TestcaseModel(this);
+//  //  new QAbstractItemModelTester(
+//  //      model, QAbstractItemModelTester::FailureReportingMode::Fatal, this);
+//}
+
+QTEST_MAIN(QTX3ModelTest)
