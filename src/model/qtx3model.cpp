842966e8ae5b60b32018863b569ba166f14e8d59
grotsztaksel <grotsztaksel@o2.pl>
2021-03-10 23:17:34 +0100

    QTX3Model: Remove "return 0" from columnCount() for the root node

--- src/model/qtx3model.cpp
+++ src/model/qtx3model.cpp
@@ -91,9 +91,6 @@ int QTX3Model::rowCount(const QModelIndex& parent) const {
 }
 
 int QTX3Model::columnCount(const QModelIndex& parent) const {
-  if (!parent.isValid())
-    return 0;
-
   return 1;
   int ic = 0;
   auto parent_node = nodeFromIndex(parent);
