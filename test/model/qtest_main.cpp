af21dccdf446c0217058cef187671943f140999a
grotsztaksel <grotsztaksel@o2.pl>
2021-03-13 22:33:32 +0100

    QTest: Collect results of all test suites and print summary

--- test/model/qtest_main.cpp
+++ test/model/qtest_main.cpp
@@ -1,22 +1,44 @@
 
+#include "qtest_main.h"
+#include <qlogging.h>
 #include <QTest>
-
 #include "qtest_qtx3model.h"
 #include "qtest_qtx3node.h"
 
 int main(int argc, char** argv) {
-  int status = 0;
-
+  QMap<QString, int> results;
   //-- run all tests
   {
     QTX3::QTX3NodeTest test_node;
-    status |= QTest::qExec(&test_node, argc, argv);
+    results["QTX3NodeTest"] = QTest::qExec(&test_node, argc, argv);
   }
 
   {
     QTX3::QTX3ModelTest test_model;
-    status |= QTest::qExec(&test_model, argc, argv);
+    results["QTX3ModelTest"] = QTest::qExec(&test_model, argc, argv);
   }
 
-  return status;
+  return printSummary(results);
+}
+
+int printSummary(QMap<QString, int> results) {
+  QString title("All tests from all suites passed");
+  QStringList failedSuites;
+  int status = 0;
+  for (auto key : results.keys()) {
+    status |= results[key];
+    if (results[key] != 0) {
+      QString s;
+      if (results[key] > 1)
+        s = "s";
+      failedSuites.append(
+          QString("%1: failed %2 test%3").arg(key).arg(results[key]).arg(s));
+    }
+  }
+  if (!failedSuites.empty()) {
+    title = QString("%1 tests failed in %2 suites:\n")
+                .arg(status)
+                .arg(failedSuites.size());
+  }
+  qInfo().noquote() << title << failedSuites.join("\n");
 }
---
+++ test/model/qtest_main.h
@@ -0,0 +1,10 @@
+#ifndef QTEST_MAIN_H
+#define QTEST_MAIN_H
+
+#include <QMap>
+#include <QString>
+
+int printSummary(const QMap<QString, int> results);
+
+
+#endif // QTEST_MAIN_H
